f944f1404e2513f7cf6befdde8c01ee782b961cb
Josh Goodwin <j@joshuagoodwin.com>
2014-02-26 21:52:32 +0000

    the state of the art, working and using sqlite

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 /**
  * Module dependencies.
  */
@@ -9,15 +10,13 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-var fs = require('fs');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 29024);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -31,7 +30,26 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-// app.get('/users', user.list);
+
+
+var fs = require("fs");
+var file = __dirname + "/fixtures.db";
+var exists = fs.existsSync(file);
+
+if(!exists) {
+  console.log("Creating DB file.");
+  fs.openSync(file, "w");
+}
+
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(file);
+
+db.serialize(function() {
+  if(!exists) {
+    db.run("CREATE TABLE Fixtures (id INTEGER PRIMARY KEY, name TEXT, time TEXT, location TEXT, pointsAvailable FLOAT, home TEXT, homePoints FLOAT, awayPoints FLOAT, awayTEXT);");
+  }
+});
+db.close();
 
 var server = http.createServer(app);
 
@@ -43,16 +61,31 @@ io = io.listen(server);
 
 io.sockets.on('connection', function(socket) {
   socket.on('edit', function(data) {
-    console.log(data);
+    var db = new sqlite3.Database(file);
+ 
+    identifiers = data[0].split('-'); // ['2', 'name']
+
+    switch (identifiers[1]) {
+      case 'name':
+      case 'location':
+      case 'time':
+      case 'pointsAvailable':
+      case 'home':
+      case 'homeScore':
+      case 'awayScore':
+      case 'away':
+        var field = identifiers[1];
+        db.serialize(function() {
+          db.run("UPDATE Fixtures SET " + field + " = $contents WHERE id = $id", {
+            $id:    identifiers[0],
+            $contents: data[1],
+          }, function(err) {
+             console.log(err);
+          });
+        });
+        break;
+    }
+    db.close();
     socket.broadcast.emit('update', data);
-
-    var dataFile = fs.readFileSync('data.json', 'utf-8');
-    var fixtures = JSON.parse(dataFile);
-
-    var identifiers = data[0].split('-'); // [ 'fixture', '2', 'time' ]
-    fixtures[identifiers[1]][identifiers[2]] = data[1];
-  
-    fs.writeFile('data.json', JSON.stringify(fixtures, null, 4), 'utf-8');
-
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,18 @@
  */
 
 var fs = require('fs');
+var file = 'fixtures.db';
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database(file);
 
 exports.index = function(req, res) {
 
-  var dataFile = fs.readFileSync('data.json', 'utf-8');
-  var fixtures = JSON.parse(dataFile);
+  db.serialize(function() {
 
-  res.render('index', {
-    title: 'Top Secret Nouse Control Panel',
-    fixtures: fixtures
+    db.all("SELECT * FROM Fixtures", function(err, rows) {
+ console.log(rows);
+      res.render('index', { fixtures: rows });
+    });
   });
-};
+
+}
