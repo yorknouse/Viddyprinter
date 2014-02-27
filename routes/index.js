28bc2a018ed7af5fe35b7f5e64ffc3d8b361b277
Josh Goodwin <j@joshuagoodwin.com>
2014-02-27 00:07:30 +0000

    handle database errors?

--- routes/index.js
+++ routes/index.js
@@ -27,41 +27,49 @@ exports.totals = function(req, res) {
 
   db.serialize(function() {
     db.all("SELECT * FROM Fixtures", function(err, fixtures) {
-    
-      // initialise accumulators to 0
-      var homePoints = 0;
-      var awayPoints = 0;
-      var maxPoints = 0;
+
+      if (err) {
+        console.error(err.stack);
+        res.send(500, 'Something broke!');
+      }
+      else {
+
+        // initialise accumulators to 0
+        var homePoints = 0;
+        var awayPoints = 0;
+        var maxPoints = 0;
       
-      for (var i = 0; i < fixtures.length; i++) {
-      	console.log(fixtures[i]);
+        for (var i = 0; i < fixtures.length; i++) {
+          if (fixtures[i].pointsAvailable) {
 
-        if (fixtures[i].pointsAvailable) {
+            maxPoints += fixtures[i].pointsAvailable;
 
-          maxPoints += fixtures[i].pointsAvailable;
-
-          if (typeof(fixtures[i].homeScore) === 'number' && typeof(fixtures[i].awayScore) === 'number') {
-            if (fixtures[i].homeScore > fixtures[i].awayScore) {
+            if (typeof(fixtures[i].homeScore) === 'number' && typeof(fixtures[i].awayScore) === 'number') {
+              if (fixtures[i].homeScore > fixtures[i].awayScore) {
                 homePoints += fixtures[i].pointsAvailable;
-            }
-            else if (fixtures[i].homeScore < fixtures[i].awayScore) {
+              }
+              else if (fixtures[i].homeScore < fixtures[i].awayScore) {
                 awayPoints += fixtures[i].pointsAvailable;
-            }
-            else {
+              }
+              else {
                 homePoints += fixtures[i].pointsAvailable / 2;
                 awayPoints += fixtures[i].pointsAvailable / 2;
+              }
             }
-          }
 
+          }
         }
+
+        res.json({
+          maxPoints: maxPoints,
+          remainingPointsAvailable: (maxPoints-homePoints-awayPoints),
+          homePoints: homePoints,
+          awayPoints: awayPoints
+        });
       }
-      res.json({
-      	maxPoints: maxPoints,
-      	remainingPointsAvailable: (maxPoints-homePoints-awayPoints),
-      	homePoints: homePoints,
-      	awayPoints: awayPoints
-      });
+
     });
+
   });
 
 }
